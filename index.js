ad3f86df2f9040e03d4e05f94ca72de5e5dfd342
FIRST_NAME LAST_NAME <MY_NAME@gmail.com>
2022-03-26 19:53:02 -0400

    added more changes to index.js

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50px'
 }
+
+footer.style.borderStyle = 'solid'
